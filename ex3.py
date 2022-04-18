153e4da8cd144559b10feee89a272b6a966e518b
faizalr <faizal.abdulkadir@gmail.com>
2022-04-18 13:02:53 +0800

    3 examples

---
+++ ex3.py
@@ -0,0 +1,38 @@
+for x in range (1,11):
+    print(x)
+ 
+print("---")    
+for x in range (1,11):
+    print(x*2)
+  
+print("---")  
+for x in range (1,11):
+    print(x*3)
+    
+print("---")  
+for x in range (1,11):
+    print(x*4)
+    
+print("---")  
+for x in range (1,11):
+    print(x*5)
+    
+print("---")  
+for x in range (1,11):
+    print(x*6)
+    
+print("---")  
+for x in range (1,11):
+    print(x*7)
+    
+print("---")  
+for x in range (1,11):
+    print(x*8)
+    
+print("---")  
+for x in range (1,11):
+    print(x*9)
+    
+print("---")  
+for x in range (1,11):
+    print(x*10)
